af00bc77a64648b4b9f1d69088079a06e92cce18
Robert Gill <rtgill82@gmail.com>
2025-11-26 13:50:13 -0800

    Fix compilation for Windows target

--- src/posix/util.rs
+++ src/posix/util.rs
@@ -1,8 +1,8 @@
 //
-// Created:  Sat Wed 26 Nov 2025 01:46:54 PM PST
-// Modified: Wed 26 Nov 2025 03:22:33 PM PST
+// Created:  Sat 18 Apr 2020 03:54:24 AM PDT
+// Modified: Sat 15 Aug 2020 08:15:49 PM PDT
 //
-// Copyright (C) 2025 Robert Gill <rtgill82@gmail.com>
+// Copyright (C) 2020 Robert Gill <rtgill82@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
@@ -24,5 +24,24 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#[cfg(target_family = "unix")]
-pub(crate) use crate::posix::util::*;
+use std::mem::MaybeUninit;
+use crate::posix::unistd;
+
+#[repr(i32)]
+pub(crate) enum BufType {
+    Group  = libc::_SC_GETGR_R_SIZE_MAX,
+    Passwd = libc::_SC_GETPW_R_SIZE_MAX
+}
+
+pub(crate) fn zeroed<T>() -> T {
+    unsafe {
+        MaybeUninit::zeroed().assume_init()
+    }
+}
+
+pub(crate) fn get_bufsize(buftype: BufType) -> usize {
+    if let Ok(rv) = unistd::sysconf(buftype as i32) {
+        return rv as usize;
+    }
+    libc::BUFSIZ as usize
+}
